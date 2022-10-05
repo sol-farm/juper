fcfd22ebbdbd9b5f433e72d05d46fae17de63989
bonedaddy <boned4ddy@protonmail.com>
2022-10-05 06:06:30 -0700

    refactor anyix swap fee

--- crates/juper_swap_api/src/anyix.rs
+++ crates/juper_swap_api/src/anyix.rs
@@ -29,6 +29,187 @@ pub const DEFAULT_MARKET_LIST: Lazy<Vec<String>> = Lazy::new(|| {
     ]
 });
 
+pub fn new_anyix_swap_with_quote(
+    swap_route: Quote,
+    rpc: &Arc<RpcClient>,
+    payer: &dyn Signer,
+    anyix_program: Pubkey,
+    management: Pubkey,
+    vault: Pubkey,
+    vault_pda: Pubkey,
+    skip_preflight: bool,
+) -> Result<()> {
+    let jup_client = crate::Client::new();
+    let swap_config = jup_client.swap_with_config(
+        swap_route,
+        vault_pda,
+        SwapConfig {
+            wrap_unwrap_sol: Some(false),
+            ..Default::default()
+        },
+    )?;
+
+    let crate::types::Swap {
+        setup,
+        swap,
+        cleanup,
+    } = swap_config;
+    if setup.is_some() {
+        log::info!("setup required");
+    }
+
+    for tx in [setup, Some(swap), cleanup]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>()
+        .iter_mut()
+    {
+        // create the legacy and sanitized messages used for processesing
+        let sanitized_msg = SanitizedMessage::Legacy(tx.message.clone());
+        let mut instructions = Vec::with_capacity(tx.message.instructions.len());
+
+        instructions.append(
+            &mut tx
+                .message
+                .instructions
+                .iter_mut()
+                .map(|compiled_ix| {
+                    Instruction::new_with_bytes(
+                        *sanitized_msg
+                            .get_account_key(compiled_ix.program_id_index.into())
+                            .ok_or(InstructionError::MissingAccount)
+                            .unwrap(),
+                        &compiled_ix.data,
+                        compiled_ix
+                            .accounts
+                            .iter()
+                            .map(|account_index| {
+                                let account_index = *account_index as usize;
+                                Ok(AccountMeta {
+                                    is_signer: sanitized_msg.is_signer(account_index),
+                                    is_writable: sanitized_msg.is_writable(account_index),
+                                    pubkey: *sanitized_msg
+                                        .get_account_key(account_index)
+                                        .ok_or(InstructionError::MissingAccount)?,
+                                })
+                            })
+                            .collect::<Result<Vec<AccountMeta>, InstructionError>>()
+                            .unwrap(),
+                    )
+                })
+                .collect::<Vec<_>>(),
+        );
+        // ensure all instructions invoke a program_id that is whitelisted
+        for ix in instructions.iter() {
+            // prevent a rogue api from returning programs that are not the ata or jupiter progarm
+            if ix.program_id.ne(&spl_associated_token_account::ID)
+                && ix.program_id.ne(&juper_swap_cpi::JUPITER_V3_AGG_ID)
+            {
+                return Err(anyhow!("invalid program id {}", ix.program_id));
+            }
+        }
+        let any_ix_args = instructions
+            .iter_mut()
+            .filter_map(|ix| {
+                // first set any signer accounts as non signers
+                // this is to prevent txn signing issues where the swap ix requires a pda to sign
+                //
+                // however if the instruction is for the ATA program we dont do this
+                if ix.program_id.eq(&juper_swap_cpi::JUPITER_V3_AGG_ID) {
+                    ix.accounts = ix
+                        .accounts
+                        .iter_mut()
+                        .map(|account| {
+                            account.is_signer = false;
+                            account.clone()
+                        })
+                        .collect();
+                    let (jup_ix, swap_input) =
+                        match juper_swap_cpi::process_jupiter_instruction(&ix.data[..]) {
+                            Ok(ix) => ix,
+                            Err(err) => {
+                                log::error!("failed to process jupiter ix {:#?}", err);
+                                return None;
+                            }
+                        };
+                    if let Ok(args) = new_jupiter_swap_ix_data(ix.clone(), jup_ix, swap_input) {
+                        Some(args)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        let mut accounts = juper_swap_cpi::accounts::JupiterSwap {
+            vault,
+            authority: payer.pubkey(),
+            jupiter_program: juper_swap_cpi::JUPITER_V3_AGG_ID,
+            management,
+        }
+        .to_account_metas(Some(true));
+        let any_ix = ::anyix::AnyIx {
+            num_instructions: any_ix_args.len() as u8,
+            instruction_data_sizes: any_ix_args.iter().map(|ix| ix.data.len() as u8).collect(),
+            instruction_account_counts: any_ix_args
+                .iter()
+                .map(|ix| ix.accounts.len() as u8)
+                .collect(),
+            instruction_datas: any_ix_args.iter().map(|ix| ix.data.clone()).collect(),
+        }
+        .pack()?;
+        accounts.extend_from_slice(
+            &any_ix_args
+                .iter()
+                .map(|ix| ix.accounts.clone())
+                .flatten()
+                .collect::<Vec<_>>()[..],
+        );
+        let ix = Instruction {
+            program_id: anyix_program,
+            accounts,
+            data: juper_swap_cpi::instructions::JupiterSwapArgs { input_data: any_ix }.data(),
+        };
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&payer.pubkey()));
+
+        #[cfg(test)]
+        println!(
+            "encoded jupiter tx {}",
+            serialize_and_encode(&tx, UiTransactionEncoding::Base64).unwrap()
+        );
+
+        tx.sign(&vec![&*payer], rpc.get_latest_blockhash()?);
+        log::info!("sending jupiter swap ix");
+        if skip_preflight {
+            match rpc.send_transaction_with_config(
+                &tx,
+                RpcSendTransactionConfig {
+                    skip_preflight,
+                    ..Default::default()
+                },
+            ) {
+                Ok(sig) => log::info!("sent jupiter swap ix {}", sig),
+                Err(err) => {
+                    let error_msg = format!("failed to send jupiter swap ix {:#?}", err);
+                    log::error!("{}", error_msg);
+                    return Err(anyhow!("{}", error_msg));
+                }
+            }
+        } else {
+            match rpc.send_and_confirm_transaction(&tx) {
+                Ok(sig) => log::info!("sent jupiter swap ix {}", sig),
+                Err(err) => {
+                    let error_msg = format!("failed to send jupiter swap ix {:#?}", err);
+                    log::error!("{}", error_msg);
+                    return Err(anyhow!("{}", error_msg));
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
 pub fn new_anyix_swap(
     rpc: &Arc<RpcClient>,
     payer: &dyn Signer,
@@ -56,15 +237,6 @@ pub fn new_anyix_swap(
             for market_info in quote.market_infos.iter() {
                 if market_list.contains(&market_info.label) {
                     continue;
-                }
-                if market_info.label.eq_ignore_ascii_case("orca (whirlpools)")
-                    || market_info.label.eq_ignore_ascii_case("orca")
-                    || market_info.label.eq_ignore_ascii_case("raydium")
-                    || market_info.label.eq_ignore_ascii_case("saber")
-                //|| market_info.label.eq_ignore_ascii_case("mercurial")
-                //|| market_info.label.eq_ignore_ascii_case("lifinity")
-                {
-                    continue;
                 } else {
                     println!("ignoring market {}", market_info.label);
                     return false;
@@ -78,183 +250,17 @@ pub fn new_anyix_swap(
     }
     for route in routes.iter().take(max_tries) {
         let swap_fn = |swap_route: Quote| -> Result<()> {
-            let jup_client = crate::Client::new();
-            let swap_config = jup_client.swap_with_config(
+            new_anyix_swap_with_quote(
                 swap_route,
+                rpc,
+                payer,
+                anyix_program,
+                management,
+                vault,
                 vault_pda,
-                SwapConfig {
-                    wrap_unwrap_sol: Some(false),
-                    ..Default::default()
-                },
-            )?;
-
-            let crate::types::Swap {
-                setup,
-                swap,
-                cleanup,
-            } = swap_config;
-            if setup.is_some() {
-                log::info!("setup required");
-            }
-
-            for tx in [setup, Some(swap), cleanup]
-                .into_iter()
-                .flatten()
-                .collect::<Vec<_>>()
-                .iter_mut()
-            {
-                // create the legacy and sanitized messages used for processesing
-                let sanitized_msg = SanitizedMessage::Legacy(tx.message.clone());
-                let mut instructions = Vec::with_capacity(tx.message.instructions.len());
-
-                instructions.append(
-                    &mut tx
-                        .message
-                        .instructions
-                        .iter_mut()
-                        .map(|compiled_ix| {
-                            Instruction::new_with_bytes(
-                                *sanitized_msg
-                                    .get_account_key(compiled_ix.program_id_index.into())
-                                    .ok_or(InstructionError::MissingAccount)
-                                    .unwrap(),
-                                &compiled_ix.data,
-                                compiled_ix
-                                    .accounts
-                                    .iter()
-                                    .map(|account_index| {
-                                        let account_index = *account_index as usize;
-                                        Ok(AccountMeta {
-                                            is_signer: sanitized_msg.is_signer(account_index),
-                                            is_writable: sanitized_msg.is_writable(account_index),
-                                            pubkey: *sanitized_msg
-                                                .get_account_key(account_index)
-                                                .ok_or(InstructionError::MissingAccount)?,
-                                        })
-                                    })
-                                    .collect::<Result<Vec<AccountMeta>, InstructionError>>()
-                                    .unwrap(),
-                            )
-                        })
-                        .collect::<Vec<_>>(),
-                );
-                // ensure all instructions invoke a program_id that is whitelisted
-                for ix in instructions.iter() {
-                    // prevent a rogue api from returning programs that are not the ata or jupiter progarm
-                    if ix.program_id.ne(&spl_associated_token_account::ID)
-                        && ix.program_id.ne(&juper_swap_cpi::JUPITER_V3_AGG_ID)
-                    {
-                        return Err(anyhow!("invalid program id {}", ix.program_id));
-                    }
-                }
-                let any_ix_args = instructions
-                    .iter_mut()
-                    .filter_map(|ix| {
-                        // first set any signer accounts as non signers
-                        // this is to prevent txn signing issues where the swap ix requires a pda to sign
-                        //
-                        // however if the instruction is for the ATA program we dont do this
-                        if ix.program_id.eq(&juper_swap_cpi::JUPITER_V3_AGG_ID) {
-                            ix.accounts = ix
-                                .accounts
-                                .iter_mut()
-                                .map(|account| {
-                                    account.is_signer = false;
-                                    account.clone()
-                                })
-                                .collect();
-                            let (jup_ix, swap_input) =
-                                match juper_swap_cpi::process_jupiter_instruction(&ix.data[..]) {
-                                    Ok(ix) => ix,
-                                    Err(err) => {
-                                        log::error!("failed to process jupiter ix {:#?}", err);
-                                        return None;
-                                    }
-                                };
-                            if let Ok(args) =
-                                new_jupiter_swap_ix_data(ix.clone(), jup_ix, swap_input)
-                            {
-                                Some(args)
-                            } else {
-                                None
-                            }
-                        } else {
-                            None
-                        }
-                    })
-                    .collect::<Vec<_>>();
-                let mut accounts = juper_swap_cpi::accounts::JupiterSwap {
-                    vault,
-                    authority: payer.pubkey(),
-                    jupiter_program: juper_swap_cpi::JUPITER_V3_AGG_ID,
-                    management,
-                }
-                .to_account_metas(Some(true));
-                let any_ix = ::anyix::AnyIx {
-                    num_instructions: any_ix_args.len() as u8,
-                    instruction_data_sizes: any_ix_args
-                        .iter()
-                        .map(|ix| ix.data.len() as u8)
-                        .collect(),
-                    instruction_account_counts: any_ix_args
-                        .iter()
-                        .map(|ix| ix.accounts.len() as u8)
-                        .collect(),
-                    instruction_datas: any_ix_args.iter().map(|ix| ix.data.clone()).collect(),
-                }
-                .pack()?;
-                accounts.extend_from_slice(
-                    &any_ix_args
-                        .iter()
-                        .map(|ix| ix.accounts.clone())
-                        .flatten()
-                        .collect::<Vec<_>>()[..],
-                );
-                let ix = Instruction {
-                    program_id: anyix_program,
-                    accounts,
-                    data: juper_swap_cpi::instructions::JupiterSwapArgs { input_data: any_ix }
-                        .data(),
-                };
-                let mut tx = Transaction::new_with_payer(&[ix], Some(&payer.pubkey()));
-
-                #[cfg(test)]
-                println!(
-                    "encoded jupiter tx {}",
-                    serialize_and_encode(&tx, UiTransactionEncoding::Base64).unwrap()
-                );
-
-                tx.sign(&vec![&*payer], rpc.get_latest_blockhash()?);
-                log::info!("sending jupiter swap ix");
-                if skip_preflight {
-                    match rpc.send_transaction_with_config(
-                        &tx,
-                        RpcSendTransactionConfig {
-                            skip_preflight,
-                            ..Default::default()
-                        },
-                    ) {
-                        Ok(sig) => log::info!("sent jupiter swap ix {}", sig),
-                        Err(err) => {
-                            let error_msg = format!("failed to send jupiter swap ix {:#?}", err);
-                            log::error!("{}", error_msg);
-                            return Err(anyhow!("{}", error_msg));
-                        }
-                    }
-                } else {
-                    match rpc.send_and_confirm_transaction(&tx) {
-                        Ok(sig) => log::info!("sent jupiter swap ix {}", sig),
-                        Err(err) => {
-                            let error_msg = format!("failed to send jupiter swap ix {:#?}", err);
-                            log::error!("{}", error_msg);
-                            return Err(anyhow!("{}", error_msg));
-                        }
-                    }
-                }
-            }
-            Ok(())
+                skip_preflight,
+            )
         };
-        // if the swap succeeds, exit the loop early
         if swap_fn(route.clone()).is_ok() {
             break;
         }
