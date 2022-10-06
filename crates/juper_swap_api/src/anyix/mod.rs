1e2641271e18ab373c63e846135ca134b1e6970f
bonedaddy <boned4ddy@protonmail.com>
2022-10-05 18:52:31 -0700

    reorganize anyix file into a more structured module

---
+++ crates/juper_swap_api/src/anyix/mod.rs
@@ -0,0 +1,350 @@
+pub mod swap;
+use crate::{
+    slippage::{FeeBps, Slippage},
+    types::{Quote, SwapConfig},
+};
+use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::InstructionData;
+use anchor_lang::{
+    prelude::AccountMeta, solana_program::instruction::InstructionError, ToAccountMetas,
+};
+use anyhow::{anyhow, Result};
+use juper_swap_cpi::{JupiterIx, SwapInputs};
+use once_cell::sync::Lazy;
+use solana_client::rpc_client::RpcClient;
+use solana_client::rpc_config::RpcSendTransactionConfig;
+use solana_sdk::instruction::Instruction;
+use solana_sdk::transaction::Transaction;
+use solana_sdk::{message::SanitizedMessage, signer::Signer};
+
+#[cfg(test)]
+use solana_client::rpc_client::serialize_and_encode;
+
+use std::{collections::HashMap, sync::Arc};
+
+pub const DEFAULT_MARKET_LIST: Lazy<Vec<String>> = Lazy::new(|| {
+    vec![
+        "orca (whirlpools)".to_string(),
+        "orca".to_string(),
+        "raydium".to_string(),
+        "saner".to_string(),
+    ]
+});
+
+/// wraps the instruction data, and instruction accounts required by an AnyIx instruction
+pub struct AnyIxArgs {
+    pub accounts: Vec<AccountMeta>,
+    pub data: Vec<u8>,
+}
+
+/// given an instruction from the jupiter swap api, encode the instruction
+/// into the AnyIx format accepted by our vaults program
+pub fn new_jupiter_swap_ix(
+    swap_api_ix: Instruction,
+    jup_ix: JupiterIx,
+    swap_input: SwapInputs,
+    anyix_program: Pubkey,
+) -> Result<Instruction> {
+    match jup_ix {
+        JupiterIx::SetTokenLedger => {
+            Ok(jup_ix.encode_token_ledger_ix(anyix_program, swap_api_ix.accounts))
+        }
+        _ => Ok(jup_ix.encode_swap_ix(swap_input, anyix_program, swap_api_ix.accounts)),
+    }
+}
+
+/// given an instruction from the jupiter swap api, encode the instruction
+/// into the AnyIx format accepted by our vaults program
+pub fn new_jupiter_swap_ix_data(
+    swap_api_ix: Instruction,
+    jup_ix: JupiterIx,
+    swap_input: SwapInputs,
+) -> Result<AnyIxArgs> {
+    let mut data: Vec<u8> = vec![jup_ix.into()];
+    if jup_ix.ne(&JupiterIx::SetTokenLedger) {
+        data.extend_from_slice(&swap_input.pack()[..]);
+    }
+    Ok(AnyIxArgs {
+        data,
+        accounts: swap_api_ix.accounts,
+    })
+}
+
+/// given an instruction `ix`, iterate over all all accounts,
+/// applying `modify_fn` which if needed replace the account output with
+/// one matching a predefined ruleset
+pub fn replace_accounts(
+    ix: &mut Instruction,
+    rpc: &Arc<RpcClient>,
+    // returns None if no modification needed
+    modify_fn: &mut dyn FnMut(
+        // the account being checked for replacement
+        &AccountMeta,
+        // the rpc used to check on-chain state
+        &Arc<RpcClient>,
+        // a hashmap used to map the account being replaced, with the account replacing it
+        &HashMap<Pubkey, Pubkey>,
+    ) -> Option<AccountMeta>,
+    replacements: &HashMap<Pubkey, Pubkey>,
+) -> Result<()> {
+    for account in ix.accounts.iter_mut() {
+        if let Some(new_acct) = modify_fn(account, rpc, &replacements) {
+            log::warn!("replacing {} with {}", account.pubkey, new_acct.pubkey);
+            *account = new_acct;
+        }
+    }
+    Ok(())
+}
+
+/// used as a `modify_fn` parameter in `replace_accounts`, this
+/// performs a basic replacement operation, matching on the account
+/// address
+pub fn replace_by_account_pubkey(
+    account: &AccountMeta,
+    rpc: &Arc<RpcClient>,
+    replacements: &HashMap<Pubkey, Pubkey>,
+) -> Option<AccountMeta> {
+    if let Some(new) = replacements.get(&account.pubkey) {
+        return Some(AccountMeta {
+            pubkey: *new,
+            is_writable: account.is_writable,
+            is_signer: account.is_signer,
+        });
+    } else {
+        return None;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anchor_lang::solana_program;
+    use simplelog::*;
+    use solana_sdk::signature::{self, Keypair};
+    use solana_sdk::signer::Signer;
+    use static_pubkey::static_pubkey;
+    use std::collections::HashMap;
+    use std::fs;
+    use std::sync::Arc;
+    use std::{fs::File, str::FromStr};
+    pub fn derive_tokena_compound_queue(vault: Pubkey, mint: Pubkey) -> (Pubkey, u8) {
+        Pubkey::find_program_address(
+            &[b"tokena_compound_queue", vault.as_ref(), mint.as_ref()],
+            &static_pubkey!("TLPv2haaXncGsurtzQb4rMnFvuPJto4mntAa51PidhD"),
+        )
+    }
+
+    pub fn derive_tokenb_compound_queue(vault: Pubkey, mint: Pubkey) -> (Pubkey, u8) {
+        Pubkey::find_program_address(
+            &[b"tokenb_compound_queue", vault.as_ref(), mint.as_ref()],
+            &static_pubkey!("TLPv2haaXncGsurtzQb4rMnFvuPJto4mntAa51PidhD"),
+        )
+    }
+
+    #[test]
+    #[allow(unused_must_use)]
+    fn test_anyix_swap_override() {
+        TermLogger::init(
+            LevelFilter::Info,
+            ConfigBuilder::new()
+                .set_location_level(LevelFilter::Error)
+                .build(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        );
+        let rpc = Arc::new(RpcClient::new("https://ssc-dao.genesysgo.net".to_string()));
+        let orca_mint = static_pubkey!("orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE");
+        let msol_mint = static_pubkey!("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So");
+        let hbb_mint = static_pubkey!("HBB111SCo9jkCejsZfz8Ec8nH7T6THF8KEKSnvwT6XK6");
+        let mnde_mint = static_pubkey!("MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey");
+        let usdh_mint = static_pubkey!("USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX");
+        let vault_pda = static_pubkey!("663B7xaCqkFKeRKbWwbzcdXoeBLwNS1k5uDFVgUkZwh9");
+        let vault = static_pubkey!("HvRLN4NtVojvM6MicHVnUWCfBZMVWY4mn147LitM27dE");
+        let management = static_pubkey!("De74LEi2qAz5Lk8XTfm7dTRrhwpJVqbCjehLZSPzKfRN");
+        let anyix_program = static_pubkey!("TLPv2haaXncGsurtzQb4rMnFvuPJto4mntAa51PidhD");
+
+        let b64_encoded_swap_ix = base64::decode("AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAoaudEwYcD83iSmOdiCYgl0FbE1EY/HTjC9O5vVR1aEHFWqc2Opgg73NeMj+N8A8YBI7KBlgAadkiYTWIQY7+9V8RhD6EtkoqBLhz59tziCckQWnJgl6UD5U8gAbmrlZLYAS5TMIJJnkcTF2Y9tnZJBMYjEdZLTelaWUxXjYxc1LQBy6joJxxNxIhqReCIF6ipi6l8dFod/NVBGlOa9DSJu5ISU5qD0vvrQhq+nyqyPEfzUf6sPd2hWARpSfIxTXxCGUV3Qo2oarR8RfanRO45fsiz96o0HIJ1mCfSLByJGBWQLj3UTm7d3XtPJYOBovoo6CwRU3hvpqEJYQE/IzLTCZ1waD1HOYC2xb6ZMp5eamugBnFs8axvo2gd01mOANYU53dHPHkhuGADDuqPZzbztsigJaTDSQgZyxNrWOAy/zmxXYyJa5D0osAqQhhSw0/YyqDhnIGj0GwDx3EdYMq7gmzQ75dsN/tO3arD+4cQKCwImHdTmOLddU/PySdwwBfo74xNweqSUKpWIXr/IcQHRrcRUO2enkrQ5Z80qSH/OUJKWKpuQ7lVrgHG91hWFUjOg8vE3WvyY/WShXVHKFExQ0POdIYrSqXHH5oP8cJLTXQbhX15dEO6iKalbaYnCU2fyQ9T66GpX3xFpsi1C4nTOMgvpYSQbxTsZEDouboxsAZm7zZUMob9Z6rKfT5aGxqXL6rHuJlQvFBgJ1Q9HioX3PftqSb72Tp8fjTN0jXGTukPmEi7RgGqM+MLi6vWxymkVBHnVH6nNSvb3qwqwbkgtxkwyjapZJyq+L06YgldeDnh+VHcaV6bxTKnkAtVK7kX3N4rKNlx7Fpp+yD9RgrKY8Abd9uHXZaGT2cvhRs7reawctIXtX1s3kTqM9YV+/wCpGu2oNYDga4vix7P151kKaci+vDfG0APaJSmfnKJlpZ8nqtoym+YRqdZn6qX1bDrJAVznq35qhT3m2r7+nOVT4g4DaF+OkJBT5FgSHGb1p2rtx3BqoRyC+KqVKo8reHmpK9feXDkiJL2QzliBZ93nohLYkTD7z3ZjZmE+kHJx8sMGvwf3Oo0dtK8U/tuext6Xa3u+NU+95O6RRSyGRvtAdAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARkeABAREgECExQVFgMEBQYCBwgXFAMJCgsCDA0ODxgBLnTPAMT8ePMSIgAAAAMBEQkCCw0LAEBCDwAAAAAAAAAAAAAAAAAIHZsMAAAAAAA=").unwrap();
+        let mut orig_txn: Transaction = bincode::deserialize(&b64_encoded_swap_ix[..]).unwrap();
+        let sanitized_msg = SanitizedMessage::Legacy(orig_txn.message().clone());
+        let mut orig_instructions = Vec::with_capacity(12);
+        orig_instructions.append(
+            &mut orig_txn
+                .message
+                .instructions
+                .iter_mut()
+                .map(|compiled_ix| {
+                    Instruction::new_with_bytes(
+                        *sanitized_msg
+                            .get_account_key(compiled_ix.program_id_index.into())
+                            .ok_or(InstructionError::MissingAccount)
+                            .unwrap(),
+                        &compiled_ix.data,
+                        compiled_ix
+                            .accounts
+                            .iter()
+                            .map(|account_index| {
+                                let account_index = *account_index as usize;
+                                Ok(AccountMeta {
+                                    is_signer: sanitized_msg.is_signer(account_index),
+                                    is_writable: sanitized_msg.is_writable(account_index),
+                                    pubkey: *sanitized_msg
+                                        .get_account_key(account_index)
+                                        .ok_or(InstructionError::MissingAccount)
+                                        .unwrap(),
+                                })
+                            })
+                            .collect::<Result<Vec<AccountMeta>, InstructionError>>()
+                            .unwrap(),
+                    )
+                })
+                .collect::<Vec<_>>(),
+        );
+        let orig_instruction = orig_instructions[0].clone();
+        let mut new_instruction = orig_instruction.clone();
+        let mut replacements = HashMap::default();
+        replacements.insert(
+            spl_associated_token_account::get_associated_token_address(&vault_pda, &usdh_mint),
+            derive_tokena_compound_queue(vault, usdh_mint).0,
+        );
+        replacements.insert(
+            spl_associated_token_account::get_associated_token_address(&vault_pda, &mnde_mint),
+            derive_tokenb_compound_queue(vault, mnde_mint).0,
+        );
+        replace_accounts(
+            &mut new_instruction,
+            &rpc,
+            &mut replace_by_account_pubkey,
+            &replacements,
+        )
+        .unwrap();
+        assert_ne!(new_instruction, orig_instruction);
+
+        for (idx, (new, old)) in new_instruction
+            .accounts
+            .iter()
+            .zip((orig_instruction.accounts))
+            .enumerate()
+        {
+            if idx == 21 {
+                assert_eq!(
+                    new.pubkey.to_string().as_str(),
+                    "ApFizqZ9hfyDEVMtMGvTVBE9hyEV7EwRvLninB4K919n"
+                );
+                assert_eq!(
+                    old.pubkey.to_string().as_str(),
+                    "6t89P5TdjKv5L9JHE8bVfb2APx8jqvxfyVytFRDRaBPG"
+                );
+            }
+            log::info!(
+                "idx {}, new_key {}, old_key {}",
+                idx,
+                new.pubkey,
+                old.pubkey
+            );
+        }
+    }
+    #[test]
+    #[allow(unused_must_use)]
+    fn test_new_anyix_swap() {
+        TermLogger::init(
+            LevelFilter::Info,
+            ConfigBuilder::new()
+                .set_location_level(LevelFilter::Error)
+                .build(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        );
+        let rpc = Arc::new(RpcClient::new("https://ssc-dao.genesysgo.net".to_string()));
+        let orca_mint = static_pubkey!("orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE");
+        let msol_mint = static_pubkey!("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So");
+        let hbb_mint = static_pubkey!("HBB111SCo9jkCejsZfz8Ec8nH7T6THF8KEKSnvwT6XK6");
+        let mnde_mint = static_pubkey!("MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey");
+        let usdh_mint = static_pubkey!("USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX");
+        /* simiulate a USDH-mSOL Whirlpool swap
+
+        * this gives ORCA, HBB, MNDE rewards + fees
+        * requires USDH-mSOL liquidity
+
+        */
+
+        let payer = Keypair::new();
+        let vault_pda = static_pubkey!("663B7xaCqkFKeRKbWwbzcdXoeBLwNS1k5uDFVgUkZwh9");
+        let vault = static_pubkey!("HvRLN4NtVojvM6MicHVnUWCfBZMVWY4mn147LitM27dE");
+        let management = static_pubkey!("De74LEi2qAz5Lk8XTfm7dTRrhwpJVqbCjehLZSPzKfRN");
+        let anyix_program = static_pubkey!("TLPv2haaXncGsurtzQb4rMnFvuPJto4mntAa51PidhD");
+        let mut replacements = HashMap::default();
+
+        replacements.insert(
+            spl_associated_token_account::get_associated_token_address(&vault_pda, &usdh_mint),
+            derive_tokena_compound_queue(vault, usdh_mint).0,
+        );
+        replacements.insert(
+            spl_associated_token_account::get_associated_token_address(&vault_pda, &mnde_mint),
+            derive_tokenb_compound_queue(vault, mnde_mint).0,
+        );
+
+        super::swap::new_anyix_swap(
+            &rpc,
+            &payer,
+            anyix_program,
+            management,
+            vault,
+            vault_pda,
+            orca_mint,
+            usdh_mint,
+            1.0,
+            false,
+            2,
+            None,
+            &replacements,
+        );
+
+        super::swap::new_anyix_swap(
+            &rpc,
+            &payer,
+            anyix_program,
+            management,
+            vault,
+            vault_pda,
+            orca_mint,
+            usdh_mint,
+            1.0,
+            false,
+            2,
+            None,
+            &Default::default(),
+        );
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        super::swap::new_anyix_swap(
+            &rpc,
+            &payer,
+            anyix_program,
+            management,
+            vault,
+            vault_pda,
+            mnde_mint,
+            msol_mint,
+            1.0,
+            false,
+            2,
+            Some(DEFAULT_MARKET_LIST.to_vec()),
+            &Default::default(),
+        );
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        super::swap::new_anyix_swap(
+            &rpc,
+            &payer,
+            anyix_program,
+            management,
+            vault,
+            vault_pda,
+            hbb_mint,
+            usdh_mint,
+            1.0,
+            false,
+            2,
+            None,
+            &Default::default(),
+        );
+    }
+}
---
+++ crates/juper_swap_api/src/anyix/swap.rs
@@ -0,0 +1,347 @@
+//! provides functions to create AnyIx jupiter swaps using the swap api
+use crate::{
+    slippage::{FeeBps, Slippage},
+    types::{Quote, SwapConfig},
+};
+use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::InstructionData;
+use anchor_lang::{
+    prelude::AccountMeta, solana_program::instruction::InstructionError, ToAccountMetas,
+};
+use anyhow::{anyhow, Result};
+use juper_swap_cpi::{JupiterIx, SwapInputs};
+use once_cell::sync::Lazy;
+use solana_client::rpc_client::RpcClient;
+use solana_client::rpc_config::RpcSendTransactionConfig;
+use solana_sdk::instruction::Instruction;
+use solana_sdk::transaction::Transaction;
+use solana_sdk::{message::SanitizedMessage, signer::Signer};
+
+#[cfg(test)]
+use solana_client::rpc_client::serialize_and_encode;
+
+use std::{collections::HashMap, sync::Arc};
+
+use super::{replace_accounts, replace_by_account_pubkey};
+
+#[derive(Clone, Default)]
+pub struct JupiterAnyIxSwap {
+    /// vector of instructions to use for the setup process, currently unused
+    pub setup: Option<Vec<Instruction>>,
+    /// the actual anyix swap instruction
+    pub swap: Option<Instruction>,
+    /// vector of instructions to use for the cleanup process, currently unused
+    pub cleanup: Option<Vec<Instruction>>,
+}
+
+/// returns the encoded Instruction
+pub fn new_anyix_swap_ix_with_quote(
+    swap_route: Quote,
+    rpc: &Arc<RpcClient>,
+    payer: &dyn Signer,
+    anyix_program: Pubkey,
+    management: Pubkey,
+    vault: Pubkey,
+    vault_pda: Pubkey,
+    replacements: &HashMap<Pubkey, Pubkey>,
+) -> Result<JupiterAnyIxSwap> {
+    let jup_client = crate::Client::new();
+    let swap_config = jup_client.swap_with_config(
+        swap_route,
+        vault_pda,
+        SwapConfig {
+            wrap_unwrap_sol: Some(false),
+            ..Default::default()
+        },
+    )?;
+
+    let crate::types::Swap {
+        setup,
+        mut swap,
+        cleanup,
+    } = swap_config;
+    let mut jup_any_ix = JupiterAnyIxSwap::default();
+    if let Some(_) = setup {
+        log::warn!("transaction setup not yet supported");
+    }
+    jup_any_ix.swap = match process_transaction(
+        rpc,
+        payer,
+        &mut swap,
+        vault,
+        anyix_program,
+        management,
+        replacements,
+    ) {
+        Ok(ix) => Some(ix),
+        Err(err) => {
+            let error_msg = format!("tx process failed {:#?}", err);
+            log::error!("{}", error_msg);
+            return Err(anyhow!("{}", error_msg));
+        }
+    };
+    if let Some(_) = cleanup {
+        log::warn!("transaction cleanup not yet supported");
+    }
+    return Ok(jup_any_ix);
+}
+
+/// creates, and sends an AnyIx jupiter swap using the given quote
+pub fn new_anyix_swap_with_quote(
+    swap_route: Quote,
+    rpc: &Arc<RpcClient>,
+    payer: &dyn Signer,
+    anyix_program: Pubkey,
+    management: Pubkey,
+    vault: Pubkey,
+    vault_pda: Pubkey,
+    skip_preflight: bool,
+    replacements: &HashMap<Pubkey, Pubkey>,
+) -> Result<()> {
+    let jup_any_ix = new_anyix_swap_ix_with_quote(
+        swap_route,
+        rpc,
+        payer,
+        anyix_program,
+        management,
+        vault,
+        vault_pda,
+        replacements,
+    )?;
+    let jup_swap_ix = if let Some(swap_ix) = jup_any_ix.swap {
+        swap_ix
+    } else {
+        return Err(anyhow!("failed to create jupiter any ix swap"));
+    };
+    let mut tx = Transaction::new_with_payer(&[jup_swap_ix], Some(&payer.pubkey()));
+
+    #[cfg(test)]
+    println!(
+        "encoded jupiter tx {}",
+        serialize_and_encode(
+            &tx,
+            solana_transaction_status::UiTransactionEncoding::Base64
+        )
+        .unwrap()
+    );
+
+    tx.sign(&vec![payer], rpc.get_latest_blockhash()?);
+    log::info!("sending jupiter swap ix");
+    if skip_preflight {
+        match rpc.send_transaction_with_config(
+            &tx,
+            RpcSendTransactionConfig {
+                skip_preflight,
+                ..Default::default()
+            },
+        ) {
+            Ok(sig) => log::info!("sent jupiter swap ix {}", sig),
+            Err(err) => {
+                let error_msg = format!("failed to send jupiter swap ix {:#?}", err);
+                log::error!("{}", error_msg);
+                return Err(anyhow!("{}", error_msg));
+            }
+        }
+    } else {
+        match rpc.send_and_confirm_transaction(&tx) {
+            Ok(sig) => log::info!("sent jupiter swap ix {}", sig),
+            Err(err) => {
+                let error_msg = format!("failed to send jupiter swap ix {:#?}", err);
+                log::error!("{}", error_msg);
+                return Err(anyhow!("{}", error_msg));
+            }
+        }
+    }
+    Ok(())
+}
+
+/// given an input and output mint, find up to `max_tries` routes
+/// which will be executed sequentially until the first one succeeds
+pub fn new_anyix_swap(
+    rpc: &Arc<RpcClient>,
+    payer: &dyn Signer,
+    anyix_program: Pubkey,
+    management: Pubkey,
+    vault: Pubkey,
+    vault_pda: Pubkey,
+    input_mint: Pubkey,
+    output_mint: Pubkey,
+    input_amount: f64,
+    skip_preflight: bool,
+    max_tries: usize,
+    allowed_market_names: Option<Vec<String>>,
+    replacements: &HashMap<Pubkey, Pubkey>,
+) -> Result<()> {
+    let market_list: Vec<String> = if let Some(list) = allowed_market_names {
+        list
+    } else {
+        super::DEFAULT_MARKET_LIST.clone()
+    }
+    .iter_mut()
+    .map(|minfo| {
+        minfo.make_ascii_lowercase();
+        minfo.to_owned()
+    })
+    .collect();
+    let quoter = crate::quoter::Quoter::new(rpc, input_mint, output_mint)?;
+    let routes = quoter
+        .lookup_routes2(input_amount, false, Slippage::FifteenBip, FeeBps::Zero)?
+        .iter_mut()
+        .filter_map(|quote| {
+            for market_info in (*quote).market_infos.iter_mut() {
+                market_info.label.make_ascii_lowercase();
+                if market_list.contains(&market_info.label) {
+                    continue;
+                } else {
+                    println!("ignoring market {}", market_info.label);
+                    return None;
+                }
+            }
+            Some(quote.clone())
+        })
+        .collect::<Vec<_>>();
+    if routes.is_empty() {
+        return Err(anyhow!("failed to find any routes"));
+    }
+    for route in routes.iter().take(max_tries) {
+        let swap_fn = |swap_route: Quote| -> Result<()> {
+            new_anyix_swap_with_quote(
+                swap_route,
+                rpc,
+                payer,
+                anyix_program,
+                management,
+                vault,
+                vault_pda,
+                skip_preflight,
+                replacements,
+            )
+        };
+        if swap_fn(route.clone()).is_ok() {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+/// processes a transaction as returned from the jupiter swap api, into a format suitable for
+/// execution with AnyIx
+pub fn process_transaction(
+    rpc: &Arc<RpcClient>,
+    payer: &dyn Signer,
+    tx: &mut Transaction,
+    vault: Pubkey,
+    anyix_program: Pubkey,
+    management: Pubkey,
+    replacements: &HashMap<Pubkey, Pubkey>,
+) -> Result<Instruction> {
+    // create the legacy and sanitized messages used for processesing
+    let sanitized_msg = SanitizedMessage::Legacy(tx.message.clone());
+    let mut instructions = Vec::with_capacity(tx.message.instructions.len());
+
+    instructions.append(
+        &mut tx
+            .message
+            .instructions
+            .iter_mut()
+            .map(|compiled_ix| {
+                Instruction::new_with_bytes(
+                    *sanitized_msg
+                        .get_account_key(compiled_ix.program_id_index.into())
+                        .ok_or(InstructionError::MissingAccount)
+                        .unwrap(),
+                    &compiled_ix.data,
+                    compiled_ix
+                        .accounts
+                        .iter()
+                        .map(|account_index| {
+                            let account_index = *account_index as usize;
+                            Ok(AccountMeta {
+                                is_signer: sanitized_msg.is_signer(account_index),
+                                is_writable: sanitized_msg.is_writable(account_index),
+                                pubkey: *sanitized_msg
+                                    .get_account_key(account_index)
+                                    .ok_or(InstructionError::MissingAccount)?,
+                            })
+                        })
+                        .collect::<Result<Vec<AccountMeta>, InstructionError>>()
+                        .unwrap(),
+                )
+            })
+            .collect::<Vec<_>>(),
+    );
+    // ensure all instructions invoke a program_id that is whitelisted
+    for ix in instructions.iter() {
+        // prevent a rogue api from returning programs that are not the ata or jupiter progarm
+        if ix.program_id.ne(&spl_associated_token_account::ID)
+            && ix.program_id.ne(&juper_swap_cpi::JUPITER_V3_AGG_ID)
+        {
+            return Err(anyhow!("invalid program id {}", ix.program_id));
+        }
+    }
+    let any_ix_args = instructions
+        .iter_mut()
+        .filter_map(|ix| {
+            // first set any signer accounts as non signers
+            // this is to prevent txn signing issues where the swap ix requires a pda to sign
+            //
+            // however if the instruction is for the ATA program we dont do this
+            if ix.program_id.eq(&juper_swap_cpi::JUPITER_V3_AGG_ID) {
+                ix.accounts = ix
+                    .accounts
+                    .iter_mut()
+                    .map(|account| {
+                        account.is_signer = false;
+                        account.clone()
+                    })
+                    .collect();
+                let (jup_ix, swap_input) =
+                    match juper_swap_cpi::process_jupiter_instruction(&ix.data[..]) {
+                        Ok(ix) => ix,
+                        Err(err) => {
+                            log::error!("failed to process jupiter ix {:#?}", err);
+                            return None;
+                        }
+                    };
+                if let Ok(args) = super::new_jupiter_swap_ix_data(ix.clone(), jup_ix, swap_input) {
+                    Some(args)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let mut accounts = juper_swap_cpi::accounts::JupiterSwap {
+        vault,
+        authority: payer.pubkey(),
+        jupiter_program: juper_swap_cpi::JUPITER_V3_AGG_ID,
+        management,
+    }
+    .to_account_metas(Some(true));
+    let any_ix = ::anyix::AnyIx {
+        num_instructions: any_ix_args.len() as u8,
+        instruction_data_sizes: any_ix_args.iter().map(|ix| ix.data.len() as u8).collect(),
+        instruction_account_counts: any_ix_args
+            .iter()
+            .map(|ix| ix.accounts.len() as u8)
+            .collect(),
+        instruction_datas: any_ix_args.iter().map(|ix| ix.data.clone()).collect(),
+    }
+    .pack()?;
+    accounts.extend_from_slice(
+        &any_ix_args
+            .iter()
+            .flat_map(|ix| ix.accounts.clone())
+            .collect::<Vec<_>>()[..],
+    );
+    let mut ix = Instruction {
+        program_id: anyix_program,
+        accounts,
+        data: juper_swap_cpi::instructions::JupiterSwapArgs { input_data: any_ix }.data(),
+    };
+    replace_accounts(&mut ix, rpc, &mut replace_by_account_pubkey, replacements)?;
+    Ok(ix)
+}
